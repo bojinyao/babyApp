6a25bf83961e1cd86c53bc1671b7d02dabec6293
Bojin Yao <bojinyao@berkeley.edu>
2019-01-18 19:33:04 -0800

    Initial Commit

---
+++ babyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  babyApp
+//
+//  Created by Bojin Yao on 1/18/19.
+//  Copyright © 2019 Bojin Yao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
